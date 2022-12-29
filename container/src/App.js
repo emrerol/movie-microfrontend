319e9c83f06cf73978117c4abc321445d9d42d92
Emre Can Erol <erolemrecan@outlook.com>
2022-12-29 20:55:40 +0300

    updated text

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>hi there everyone</h1>
+      <h1>hi there everyone again</h1>
       <hr />
       <MarketingApp />
     </div>
